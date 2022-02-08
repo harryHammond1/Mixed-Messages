11df3b15b447d7f0ba5d2327cc9c695b346cc2a8
harryHammond1 <harryhmmnd@gmail.com>
2022-02-08 17:06:27 +0000

    added variables and onclick for button

--- MMjs.js
+++ MMjs.js
@@ -1,1 +1,11 @@
 console.log('Jokes will be here')
+const animals = ['Dog', 'Chicken', 'Cat', 'Rooster', 'Elephant', 'Hippo', 'Velociraptor', 'Slug']
+const geography = ['Bridge', 'Road', 'Channel', 'Aquaduct', 'River', 'Pacific Ocean', 'Danube']
+const answers = ['To get to the other side', 'Because he was hungry', 'Why the hell do you care?', 'Its absolutely none of your business', 'Because we live in a free society']
+
+const jokeGenerator = () => {
+const species = animals[Math.floor(Math.random() * animals.length)];
+const geog = geography[Math.floor(Math.random() * geography.length)];
+const punchline = answers[Math.floor(Math.random() * answers.length)];
+
+}
