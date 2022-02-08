b2362dc0bd85fe5466f2c402dbc2aebdfbbec7c0
harryHammond1 <harryhmmnd@gmail.com>
2022-02-08 17:21:08 +0000

    finished project

--- MMjs.js
+++ MMjs.js
@@ -1,4 +1,3 @@
-console.log('Jokes will be here')
 const animals = ['Dog', 'Chicken', 'Cat', 'Rooster', 'Elephant', 'Hippo', 'Velociraptor', 'Slug']
 const geography = ['Bridge', 'Road', 'Channel', 'Aquaduct', 'River', 'Pacific Ocean', 'Danube']
 const answers = ['To get to the other side', 'Because he was hungry', 'Why the hell do you care?', 'Its absolutely none of your business', 'Because we live in a free society']
@@ -7,5 +6,7 @@ const jokeGenerator = () => {
 const species = animals[Math.floor(Math.random() * animals.length)];
 const geog = geography[Math.floor(Math.random() * geography.length)];
 const punchline = answers[Math.floor(Math.random() * answers.length)];
-
+let result = 'Why did the ' + species + ' cross the ' + geog + '? ' + punchline
+return document.getElementById("joke").innerText=result
 }
+
